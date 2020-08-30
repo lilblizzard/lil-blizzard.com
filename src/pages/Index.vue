3e3f9c1f9b40e855172e153d1f81e33c9f93fdcb
lilblizzard <lilblizzard97@gmail.com>
2020-08-30 17:35:47 -0400

    add image and display on index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <h1>Patrick Perkins</h1>
-
+    <g-image src="~/images/profile.jpg" width="500" />
     <p>a.k.a lil-blizzard. composer, programmer, designer based in philly. currently working with old books and new tech.</p>
   </Layout>
 </template>
