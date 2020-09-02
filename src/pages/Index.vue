653c896a2022ec5eb322852bdfa58cdf33c2f52a
lilblizzard <lilblizzard97@gmail.com>
2020-09-02 11:00:45 -0400

    WIP: styling hero image

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <h1>Patrick Perkins</h1>
-    <g-image src="~/images/profile.jpg" width="500" />
+    <g-image class="hero" src="~/images/profile.jpg" width="500" />
     <p>a.k.a lil-blizzard. composer, programmer, designer based in philly. currently working with old books and new tech.</p>
   </Layout>
 </template>
@@ -17,5 +17,10 @@ export default {
 <style>
 .home-links a {
   margin-right: 1rem;
+}
+.hero {
+  border: 5px solid red;
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
