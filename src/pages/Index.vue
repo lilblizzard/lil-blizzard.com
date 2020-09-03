a7ba5a634b383421b768b13f36c145a741e6a4c2
lilblizzard <lilblizzard97@gmail.com>
2020-09-03 10:19:24 -0400

    added some styling to hero__text and changed font size to use rem

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,27 +1,36 @@
 <template>
   <Layout>
     <h1>Patrick Perkins</h1>
-    <g-image class="hero" src="~/images/profile.jpg" />
-    <p>a.k.a lil-blizzard. composer, programmer, designer based in philly. currently working with old books and new tech.</p>
+    <g-image class="hero__image" src="~/images/profile.jpg" />
+    <p class="hero__text">
+      a.k.a lil-blizzard. composer, programmer, designer based in philly.
+      currently working with old books and new tech.
+    </p>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: "Home",
-  },
-};
+    title: 'Home'
+  }
+}
 </script>
 
 <style>
 .home-links a {
   margin-right: 1rem;
 }
-.hero {
+.hero__image {
   max-width: 100%;
   max-height: 20rem;
   object-fit: cover;
   border-radius: 0.5rem;
+}
+.hero__text {
+  font-size: 1.3rem;
+  border: 1px solid rgba(83, 83, 83, 0.1);
+  border-radius: 0.5rem;
+  padding: 0.5rem;
 }
 </style>
